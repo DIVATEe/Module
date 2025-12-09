69d927b35746384d0815a75166cb7652e2dccbef
Amit_Divate <adivate49@gmail.com>
2025-12-09 22:39:38 +0530

    changes in variale.tf

--- main/ec2/variable.tf
+++ main/ec2/variable.tf
@@ -1,18 +1,18 @@
 variable "web_instance_type" {
-  default = "t3.micro"
+ 
 }
 variable "web_ami" {
-  default = "ami-02b8269d5e85954ef"
+ 
 }
 variable "web_key_name" {
-  default = "Oct30"
+  
 }
 variable "web_subnet_id" {
-  default = ""
+  
 }
 variable "web_vpc_security_group_ids" {
-  default = ""
+  
 }
 variable "web_diable_api_termination" {
-  default = false
+ 
 }
